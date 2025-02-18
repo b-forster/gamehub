5495c616c7e441e525bfe4fc6aac591a2668ee79
Brianna Forster <briannaforster@gmail.com>
2025-02-17 23:07:12 -0800

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react"
+
 function App() {
 
   return (
-    <div></div>
+    <Grid templateAreas={{
+      base: `'nav' 'main'`,
+      lg: `'nav nav' 'aside main'`,
+
+    }}>
+
+      <GridItem area='nav' bg='coral'>Nav</GridItem>
+      <Show above='lg'>
+        <GridItem area='aside' bg='gold'>Aside</GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    </Grid >
   )
 }
 
