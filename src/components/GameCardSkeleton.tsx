9b5086bff024368e860180235cca3a74a51545c4
Brianna Forster <briannaforster@gmail.com>
2025-02-22 20:08:09 -0800

    Show loading skeletons

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,12 @@
+import { Card, CardBody, Skeleton } from '@chakra-ui/react'
+
+const GameCardSkeleton = () => {
+    return (
+        <Card width='300px' borderRadius={10} overflow='hidden'>
+            <Skeleton height={'200px'}></Skeleton>
+            <CardBody></CardBody>
+        </Card>
+    )
+}
+
+export default GameCardSkeleton
