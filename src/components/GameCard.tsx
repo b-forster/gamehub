15dbb776b770b18f1e91087553be48e4286fcc99
Brianna Forster <briannaforster@gmail.com>
2025-02-22 19:34:55 -0800

    Display Metacritic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "@/hooks/useGames";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconsList from "./PlatformIconsList";
+import CriticScore from "./CriticScore";
 
 interface GameProps {
     game: Game;
@@ -12,7 +13,10 @@ const GameCard = ({ game }: GameProps) => {
             <Image src={game.background_image} />
             <CardBody>
                 <Heading fontSize='2xl'>{game.name}</Heading>
-                <PlatformIconsList platforms={game.parent_platforms.map(p => p.platform)} />
+                <HStack justifyContent='space-between'>
+                    <PlatformIconsList platforms={game.parent_platforms.map(p => p.platform)} />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
             </CardBody>
         </Card>
     )
